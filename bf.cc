624e932aba00f6a5e8b87b4db2869061fc9faca6
jiwang <jiwang@localhost.localdomain>
2024-09-26 22:31:07 +0800

    add c++ bf interpreter

---
+++ bf.cc
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <cstdlib>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <stack>
+
+constexpr static int MEMORY_SIZE = 32768;
+
+template <class... Types> void panic(std::string_view msg, Types &&...args) {
+  std::cerr << std::vformat(msg, std::make_format_args(args...));
+  exit(EXIT_FAILURE);
+}
+
+auto read_file(std::string_view path) -> std::string {
+  constexpr auto read_size = std::size_t(4096);
+  auto stream = std::ifstream(path.data());
+  stream.exceptions(std::ios_base::badbit);
+
+  if (not stream) {
+    panic("file not exist.");
+  }
+
+  auto out = std::string();
+  auto buf = std::string(read_size, '\0');
+  while (stream.read(&buf[0], read_size)) {
+    out.append(buf, 0, stream.gcount());
+  }
+  out.append(buf, 0, stream.gcount());
+  return out;
+}
+
+auto main(int argc, char **argv) -> int {
+  char memory[MEMORY_SIZE] = {0};
+  std::stack<int> stack;
+  int pointer = 0;
+
+  if (argc != 2) {
+    panic("usage: bf <filename>");
+  }
+  auto source = read_file(argv[1]);
+
+  for (int cursor = 0; cursor < source.size(); cursor++) {
+    if (pointer < 0 || pointer >= MEMORY_SIZE) {
+      panic("null pointer at {}\n", pointer);
+    }
+    char c = source[cursor];
+    switch (c) {
+    case '+':
+      memory[pointer]++;
+      break;
+    case '-':
+      memory[pointer]--;
+      break;
+    case '>':
+      pointer++;
+      break;
+    case '<':
+      pointer--;
+      break;
+    case ',':
+      memory[pointer] = getchar();
+      break;
+    case '.':
+      putchar(memory[pointer]);
+      break;
+    case '[':
+      stack.push(cursor);
+      if (not memory[pointer]) {
+        while (source[cursor] != ']' && cursor < source.size()) {
+          cursor++;
+        }
+      }
+      break;
+    case ']':
+      if (stack.empty()) {
+        panic("pop from empty stack\n");
+      }
+      if (memory[pointer]) {
+        cursor = stack.top();
+      } else {
+        stack.pop();
+      }
+      break;
+    default:
+      break;
+    }
+  }
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	clang++ -std=c++20 -g -fsanitize=address bf.cc
